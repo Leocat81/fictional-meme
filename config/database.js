c0c5ad819bab793ca2aae42bb487a1cf8743febb
pipe <1173882893@qq.com>
2020-07-19 00:28:17 +0800

    修改

---
+++ config/database.js
@@ -0,0 +1,11 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'root',
+        'password': '123456',
+        'port': '3306',
+    },
+	'database': 'my_schema',
+    'users_table': 'users'
+};
